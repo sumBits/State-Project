67388d2b9d0b59196deff57f66fd68a8119a1637
WhiteDeath820 <allensam0@gmail.com>
2015-10-13 22:11:07 -0600

    started on auth and account creation

--- website/js/controllers.js
+++ website/js/controllers.js
@@ -3,7 +3,7 @@
 
 //update now it grabs from directives because nothing uses directives but controllers i think
 //ill have to check but it doesnt matter it will work (maybe) either way
-var webctrl = angular.module('app.controllers', ['app.controllers.directives'])
+var webctrl = angular.module('app.controllers', ['app.controllers.directives', 'firebase'])
 
 webctrl.controller('homeCtrl', function () {
 
@@ -13,7 +13,47 @@ webctrl.controller('acountCtrl', function () {
 
 })
 
-webctrl.controller('signupCtrl', function () {
+webctrl.controller('signupCtrl', function ($firebase, fireBaseData) {
+  //connects to database
+  var ref = new Firebase("firebaselinkthing");
+  var sync = $firebase(ref);
+
+  //scopes the page to find createAccount
+  //creates account
+  $scope.createAccount = function (usn, em, pwd) {
+    //check for username existance here
+
+    //creates cookie with username
+    put('User_Name_Cookie', usn);
+
+    //random generate a key here
+    function Random_Gen_Key() {
+
+      var Key = thing
+      return Key
+    }
+    
+    var User_Data = {
+      'Username': usn,
+      'secret': Key
+    }
+    put(Key, User_Data)
+
+        //creates account in firebase
+        //allowed to edit data stuff
+        fireBaseData.ref().createUser({
+            email: em,
+            password: pwd
+        }, function (error) {
+            if (error == null) {
+                console.log("User created successfully.");
+                loginFunction(em, pwd);
+            } else {
+                console.log("Error creating user: ", error);
+            }
+        });
+    };
+
 
 })
 //this controller is not directly linked to a webpage
