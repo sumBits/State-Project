593173296293ca3196c4428e526b93e14be315df
WhiteDeath820 <allensam0@gmail.com>
2015-10-21 09:56:36 -0600

    added some servies (unfinished)

--- website/js/services.js
+++ website/js/services.js
@@ -1,5 +1,14 @@
+//services are basically private functions and connecting to outside sources
+// like servers, to call a funtion you do 
 angular.module('app.services', []);
 
 .factory('firebaseService', function () {
+    return {
+        check_if_logged_in: function () {
 
-})
+        },
+        create_user_cookie: function () {
+
+        }
+    }
+});
