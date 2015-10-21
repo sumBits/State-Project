aa5861f20d201bad823deaa406d4adea960db1a7
WhiteDeath820 <allensam0@gmail.com>
2015-10-20 21:07:26 -0600

    Cleaned code up

--- website/js/app.js
+++ website/js/app.js
@@ -3,38 +3,38 @@ var website = angular.module('app', ['app.controllers', 'app.services', 'ui.rout
 
 
 website.run(function ($state, $rootScope, $window, $stateParams) {
-  //code that runs when website is first opened
+    //code that runs when website is first opened
 
-  //here will be a function that checks to see if any login cookie exists if not login page
-  //otherwise the fall back is the home page (code located at bottom of file)
+    //here will be a function that checks to see if any login cookie exists if not login page
+    //otherwise the fall back is the home page (code located at bottom of file)
 
 //  if (/*userlogin status here */ === true) {
 //    $state.go('home')
 //  } else {
 //    $state.go('signup')
 //  }
-  $rootScope.$state = $state;
-  $rootScope.$stateParams = $stateParams;
-    // ui-sref='signup'
+//
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    $state.go('signup');
     // $state.go('whoopsies')
     // $state.go('home')
-    // $state.go('acount')
-  //checks if user is online or not
-  $rootScope.online = navigator.onLine;
-      $window.addEventListener("offline", function () {
-        $rootScope.$apply(function() {
-          $rootScope.online = false; //it scopes all files for an html thing that says {{online}} it will change {{online}}  to true if online and false if offline
+    //checks if user is online or not
+    $rootScope.online = navigator.onLine;
+    $window.addEventListener("offline", function () {
+        $rootScope.$apply(function () {
+            $rootScope.online = false; //it scopes all files for an html thing that says {{online}} it will change {{online}}  to true if online and false if offline
         });                            //cane change message later by changing true or false
-      }, false);
-      $window.addEventListener("online", function () {
-        $rootScope.$apply(function() {
-          $rootScope.online = true;
+    }, false);
+    $window.addEventListener("online", function () {
+        $rootScope.$apply(function () {
+            $rootScope.online = true;
         });
-      }, false);
+    }, false);
 });
 
 //url location of html files
-                        //these are providers in the angular ui https://github.com/angular-ui/ui-router/wiki/URL-Routing
+//these are providers in the angular ui https://github.com/angular-ui/ui-router/wiki/URL-Routing
 
 //PLEASE NOTE: this does not mean that index.html has to be the main page!!
 //in fact you can make all the pages one page but seprate html files so that the whole website loads on the inital load
@@ -42,49 +42,48 @@ website.run(function ($state, $rootScope, $window, $stateParams) {
 
 website.config(function ($stateProvider, $urlRouterProvider, $locationProvider) { //enables html5 so that routing looks better
 
-  $locationProvider
-    .html5Mode(
-      {
-        enabled: true,
-        requireBase: false
-      })
-    .hashPrefix('!');
+    $locationProvider
+        .html5Mode(
+        {
+            enabled: true,
+            requireBase: false
+        })
+        .hashPrefix('!');
 
 
-  //calles the stateProvider which is basically a thing that handles the state of the html (what page it is on)
+    //calles the stateProvider which is basically a thing that handles the state of the html (what page it is on)
 
-  //the html pages locations are always in template the way you work it is:
-  // 1. make the html file
-  // 2. make a route in website.config
-  // 3. add a controller or existing controller
+    //the html pages locations are always in template the way you work it is:
+    // 1. make the html file
+    // 2. make a route in website.config
+    // 3. add a controller or existing controller
 
-  //controllers are what run what is on the html pages like buttons logins etc
-  //for more info refer to the above link
-  $stateProvider
-    //state for homepage don't put semicolon until last .state
-    .state('home', {
-      url: '/home', //this is like www.websitename.com/home
-      templateUrl: '/templates/home.html',
-      controller: 'homeCtrl'
-    })
-    .state('signup', {
-      url: '/', //bascially like www.websitename.com with nothing at the end
-      templateUrl: '/templates/signup.html',
-      controller: 'signupCtrl'
-    })
-    .state('account', {
-      url: '/account',
-      templateUrl: '/templates/account.html',
-      controller: 'accountCtrl'
-    })
-    .state('whoopsies', {
-      url:'/whoopsies',
-      templateUrl: '/templates/whoopsies.html',
-      controller: 'fourofourCtrl'
-    });
+    //controllers are what run what is on the html pages like buttons logins etc
+    //for more info refer to the above link
+    $stateProvider
+        //state for homepage don't put semicolon until last .state
+        .state('home', {
+            url: '/home', //this is like www.websitename.com/home
+            templateUrl: '/templates/home.html',
+            controller: 'homeCtrl'
+        })
+        .state('signup', {
+            url: '/', //bascially like www.websitename.com with nothing at the end
+            templateUrl: '/templates/signup.html',
+            controller: 'signupCtrl'
+        })
+        .state('account', {
+            url: '/account',
+            templateUrl: '/templates/account.html',
+            controller: 'accountCtrl'
+        })
+        .state('whoopsies', {
+            url: '/whoopsies',
+            templateUrl: '/templates/whoopsies.html',
+            controller: 'fourofourCtrl'
+        });
 
 
-
-  //fallback state
-  // $urlRouterProvider.otherwise('/whoopsies');
+    //fallback state
+    // $urlRouterProvider.otherwise('/whoopsies');
 });
--- website/js/controllers.js
+++ website/js/controllers.js
@@ -5,64 +5,64 @@
 //ill have to check but it doesnt matter it will work (maybe) either way
 angular.module('app.controllers', ['firebase'])
 
-.controller('fourofourCtrl', function() {
+    .controller('fourofourCtrl', function () {
 
-})
+    })
 
-.controller('homeCtrl', function () {
+    .controller('homeCtrl', function () {
 
-})
+    })
 
-.controller('accountCtrl', function () {
+    .controller('accountCtrl', function () {
 
-})
+    })
 
-.controller('signupCtrl', function ($firebase, $scope) {
-  //connects to database
-  var ref = new Firebase("https://sumbits-scroller.firebaseio.com/");
-  var sync = $firebase(ref);
+    .controller('signupCtrl', function ($firebase, $scope) {
+        //connects to database
+        var ref = new Firebase("https://sumbits-scroller.firebaseio.com/");
+        var sync = $firebase(ref);
 
-  //scopes the page to find createAccount
-  //creates account
-  $scope.createAccount = function (usn, em, pwd) {
-    //check for username existance here
+        //scopes the page to find createAccount
+        //creates account
+        $scope.createAccount = function (usn, em, pwd) {
+            //check for username existance here
 
-    //creates cookie with username
-    put('User_Name_Cookie', usn);
+            //creates cookie with username
+            put('User_Name_Cookie', usn);
 
-    //random generate a key here
-    function Random_Gen_Key() {
+            //random generate a key here
+            function Random_Gen_Key() {
 
-      var Key = thing;
-      return Key
-    }
+                var Key = thing;
+                return Key
+            }
 
-    var User_Data = {
-      'Username': usn,
-      'secret': Key
-    };
-    put(Key, User_Data);
+            var User_Data = {
+                'Username': usn,
+                'secret': Key
+            };
+            put(Key, User_Data);
 
-        // //creates account in firebase
-        // //allowed to edit data stuff
-        // fireBaseData.ref().createUser({
-        //     email: em,
-        //     password: pwd
-        // }, function (error) {
-        //     if (error == null) {
-        //         console.log("User created successfully.");
-        //         loginFunction(em, pwd);
-        //     } else {
-        //         console.log("Error creating user: ", error);
-        //     }
-        // });
-    };
+            // //creates account in firebase
+            // //allowed to edit data stuff
+            // fireBaseData.ref().createUser({
+            //     email: em,
+            //     password: pwd
+            // }, function (error) {
+            //     if (error == null) {
+            //         console.log("User created successfully.");
+            //         loginFunction(em, pwd);
+            //     } else {
+            //         console.log("Error creating user: ", error);
+            //     }
+            // });
+        };
 
 
-})
+    })
 //this controller is not directly linked to a webpage
 //this way it is accessable by any element
 // to use the controller in a element do <(element) ng-controller="animationCtrl">
-.controller('animationCtrl', function () {
+    .controller('animationCtrl', function () {
 
-});
+    });
